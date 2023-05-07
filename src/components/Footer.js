58934c986b8e2784df0dd3a240129c89d50083d9
igee-17 <igbagboleye@gmail.com>
2023-05-07 08:58:15 +0100

    footer in progress

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { footer } from "../utils/data";
+import facebook from "../assets/images/facebook.svg";
+import instagram from "../assets/images/instagram.svg";
+import twitter from "../assets/images/twitter.svg";
+import linkedin from "../assets/images/linkedin.svg";
+import phoneTransparent from "../assets/images/phoneTransparent.svg";
+import mailTransparent from "../assets/images/mailTransparent.svg";
 
 const Footer = () => {
   return (
@@ -18,6 +24,43 @@ const Footer = () => {
             </div>
           );
         })}
+        <div className="item">
+          <h2>connect with us</h2>
+          <div className="bottom">
+            <p>subscribe to our newsletter</p>
+            <div className="input">
+              <input type="text" placeholder="Email Address" />
+              <button>subscribe</button>
+            </div>
+            <p>
+              <span>
+                <img src={phoneTransparent} alt="phoneTransparent" />
+              </span>{" "}
+              + 234-(1)-271-6899
+            </p>
+            <p>
+              <span>
+                <img src={mailTransparent} alt="mailTransparent" />
+              </span>{" "}
+              info@panafricancapitalholdings.com
+            </p>
+            <h3>follow us</h3>
+            <div className="icons">
+              <div className="icon">
+                <img src={facebook} alt="facebook" />
+              </div>
+              <div className="icon">
+                <img src={instagram} alt="instagram" />
+              </div>
+              <div className="icon">
+                <img src={twitter} alt="twitter" />
+              </div>
+              <div className="icon">
+                <img src={linkedin} alt="linkedin" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
