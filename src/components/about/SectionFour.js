10d8079f20677f4c596a3616b4ee2a57cc8c0b12
igee-17 <igbagboleye@gmail.com>
2023-05-09 09:22:49 +0100

    team in progress

--- src/components/about/SectionFour.js
+++ src/components/about/SectionFour.js
@@ -1,4 +1,5 @@
 import React from "react";
+import businessMan from "../../assets/images/businessMan.png";
 
 const SectionFour = () => {
   return (
@@ -9,12 +10,54 @@ const SectionFour = () => {
         <div className="bottom">
           <p>board of directors</p>
           <div className="items">
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Dolapo Atekoja</h2>
+                <p>Chairman</p>
+                <button>read bio</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Chris Oshiafi</h2>
+                <p>Chief Executive Officer</p>
+                <button>read bio</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Sina Alimi</h2>
+                <p>Deputy Chief Executive Officer</p>
+                <button>read bio</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Eric Okoruwa</h2>
+                <p>Deputy Chief Executive Officer</p>
+                <button>read bio</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Eric Okoruwa</h2>
+                <p>Deputy Chief Executive Officer</p>
+                <button>read bio</button>
+              </div>
+            </div>
+            <div className="item">
+              <img src={businessMan} alt="businessMan" />
+              <div className="text">
+                <h2>Eric Okoruwa</h2>
+                <p>Deputy Chief Executive Officer</p>
+                <button>read bio</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
