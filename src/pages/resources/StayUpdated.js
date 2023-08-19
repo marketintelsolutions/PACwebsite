0b01fa27b50c9c5c4a2347a4d1726959e13c17c5
igee-17 <igbagboleye@gmail.com>
2023-08-19 12:36:28 +0100

    blod displaying in progress

--- src/pages/resources/StayUpdated.js
+++ src/pages/resources/StayUpdated.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SectionOne from "../../components/about/SectionOne";
 import vendorbg from "../../assets/images/vendorbg.png";
 import { foundations } from "../../utils/data";
@@ -6,11 +6,31 @@ import Pagination from "../../components/resources/Pagination";
 import { Link } from "react-router-dom";
 import LiquidBackground from "../../components/LiquidBackground";
 import { Translate } from "react-auto-translate";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 
 const StayUpdated = () => {
+  const [postLists, setPostLists] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const postCollectionRef = collection(db, "stayUpdated");
+
+  useEffect(() => {
+    setLoading(true);
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false);
+    };
+
+    return () => getPosts();
+  }, []);
+
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
+
+  // if (loading) return <h2>Loading...</h2>;
 
   return (
     <section className="stay-updated">
@@ -21,20 +41,20 @@ const StayUpdated = () => {
       <div className="section-two">
         <div className="foundations-container">
           <div className="foundations">
-            {foundations.map((item, index) => {
-              const { text, image } = item;
+            {postLists.map((item, index) => {
+              const { id, header, imgUrl } = item;
               return (
                 <div
                   className="item"
                   key={index}
-                  style={{ backgroundImage: `url(${image})` }}
+                  style={{ backgroundImage: `url(${imgUrl})` }}
                 >
                   <div className="text">
                     <span></span>
                     <p>
-                      <Translate>{text}</Translate>
+                      <Translate>{header}</Translate>
                     </p>
-                    <Link to={`/resources/stay-updated/${text}`}>
+                    <Link to={`/resources/stay-updated/${id}`}>
                       <Translate> Read more</Translate>
                     </Link>
                   </div>
--- src/pages/resources/StayUpdatedItem.js
+++ src/pages/resources/StayUpdatedItem.js
@@ -1,13 +1,33 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import arrowRight from "../../assets/logos/arrowRight.svg";
+import fashion from "../../assets/images/fashion.png";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
+import moment from "moment";
 
 const StayUpdatedItem = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [blog, setBlog] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    id && getBlogDetails();
+  }, []);
+
   useEffect(() => {
     window.scroll(0, 0);
   }, []);
 
-  const navigate = useNavigate();
+  const getBlogDetails = async () => {
+    const docRef = doc(db, "stayUpdated", `${id}`);
+    const blogDetail = await getDoc(docRef);
+    setBlog(blogDetail.data());
+    setLoading(false);
+  };
+  // console.log(blog, "blog");
 
   const handleGoBack = () => {
     navigate(-1); // Go back one step in the history
@@ -15,18 +35,29 @@ const StayUpdatedItem = () => {
 
   return (
     <section className="stay-updated-item">
-      <div className="content">
-        <div className="btn-container">
-          <button onClick={handleGoBack}>
-            Stay Updated <img src={arrowRight} alt="arrowRight" />{" "}
-          </button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="content">
+          <div className="btn-container">
+            <button onClick={handleGoBack}>
+              Stay Updated <img src={arrowRight} alt="arrowRight" />{" "}
+            </button>
+          </div>
+          <h1>{blog.header}</h1>
+          <p className="date">{moment(blog.date).format("MMM Do YYYY")}</p>
+          <div className="image">
+            <img src={blog.imgUrl} alt="fashion" />
+            <span>{blog.imageTitle}</span>
+          </div>
+          <div className="short-desc">
+            <p>{blog.titleDesc}</p>
+          </div>
+          <div className="body">
+            <p>{blog.body}</p>
+          </div>
         </div>
-        <h1>
-          PAC Capital Commits to a 3-year Sponsorship Agreement with CANEX
-          presents Africa @Portugal Fashion Week
-        </h1>
-        <p className="date">27th March 2023</p>
-      </div>
+      )}
     </section>
   );
 };
