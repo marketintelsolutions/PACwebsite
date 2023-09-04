ab241de3692576f3c31f5e4919ff9f5551936caf
igee-17 <igbagboleye@gmail.com>
2023-09-04 22:48:10 +0100

    env changed

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -11,12 +11,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBeLsOasXSoR2BV2CKwvG0KBLCzWFy6bIg",
-  authDomain: "pac-website-ee788.firebaseapp.com",
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
   projectId: "pac-website-ee788",
   storageBucket: "pac-website-ee788.appspot.com",
   messagingSenderId: "135437435413",
-  appId: "1:135437435413:web:6653886fc0ff0487b3041c",
+  appId: `${process.env.FIREBASE_API_ID}`,
   measurementId: "G-TWY3LNQ21T",
 };
 
