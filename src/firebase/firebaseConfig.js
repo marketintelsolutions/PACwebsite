505af89a87e74a45bd73546f12201929b9e4ccd5
igee-17 <igbagboleye@gmail.com>
2023-09-15 08:53:31 +0100

    credentials

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -12,12 +12,15 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  // apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  apiKey: `AIzaSyBeLsOasXSoR2BV2CKwvG0KBLCzWFy6bIg`,
+  // authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  authDomain: `AIzaSyBeLsOasXSoR2BV2CKwvG0KBLCzWFy6bIg`,
   projectId: "pac-website-ee788",
   storageBucket: "pac-website-ee788.appspot.com",
   messagingSenderId: "135437435413",
-  appId: `${process.env.REACT_APP_FIREBASE_API_ID}`,
+  // appId: `${process.env.REACT_APP_FIREBASE_API_ID}`,
+  appId: `1:135437435413:web:6653886fc0ff0487b3041c`,
   measurementId: "G-TWY3LNQ21T",
 };
 
