dbbadf6643f8f52af611dfefb1bb282ec8a74f05
igee-17 <igbagboleye@gmail.com>
2024-01-22 00:50:14 +0100

    pdf route added

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-// const express = require('express');
 const router = express.Router();
-// const nodemailer = require("nodemailer");
 const cors = require("cors");
-// const path = require("path");
 const sendMail = require("./modules/sendEmail");
 
 const app = express();
@@ -11,7 +8,7 @@ const app = express();
 // app.use(express.static(path.join(__dirname + "/public")));
 
 const port = process.env.PORT || 8080;
-// require("dotenv").config();
+require("dotenv").config();
 
 // allows to send information from frontend to backend
 app.use(cors());
@@ -23,16 +20,36 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname), "public", "index.html");
+  //   res.sendFile(path.join(__dirname), "public", "index.html");
 
-//   sendEmail()
-//     .then((response) => res.send(response.message))
-//     .catch((error) => res.status(500).send(error.message));
-res.send({message:'connection successful'})
+  //   sendEmail()
+  //     .then((response) => res.send(response.message))
+  //     .catch((error) => res.status(500).send(error.message));
+  res.send({ message: 'connection successful' })
 });
 
-router.post('/send-email', sendMail);
+app.post('/send-email', sendMail);
+
+app.get('/getPdf', async (req, res) => {
+  const downloadUrl = req.query.downloadUrl;
+  console.log(downloadUrl);
+  console.log('request for pdf made');
+  try {
+    const pdfResponse = await fetch(downloadUrl);
+
+    const pdfArrayBuffer = await pdfResponse.arrayBuffer();
+
+    // Convert ArrayBuffer to Buffer
+    const pdfBuffer = Buffer.from(pdfArrayBuffer);
+
+    res.contentType('application/pdf');
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, () => {
-    console.log(`server is listening on port ${port}`);
+  console.log(`server is listening on port ${port}`);
 })
